78a88c3b4012c0800d830821fda18e4a695b2550
robertf0 <robertf0000@gmail.com>
2015-05-11 12:55:00 -0700

    Revised code

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,7 @@
+cards = %w{Ace King Queen Jack 10 9 8 7 6 5 4 3 2}
+suits = %w{Spades Clubs Hearts Diamonds}
+DECK = cards.product(suits)
+
 def say(statement)
   puts "<<<<< #{statement} >>>>>"
 end
@@ -5,11 +9,6 @@ end
 def ask(question)
   puts "????? #{question} ?????"
 end
-
-cards = %w{Ace King Queen Jack 10 9 8 7 6 5 4 3 2}
-suits = %w{Spades Clubs Hearts Diamonds}
-
-DECK = cards.product(suits)
 
 def calculate_total(cards)
   arr = cards.map{|c| c[0]}
@@ -32,15 +31,53 @@ def calculate_total(cards)
   total
 end
 
-def blackjack?(p_total, d_total)
-  if d_total == 21 && p_total == 21
+def print_dealers_hand(dealer_cards, dealer_total)
+  say "Dealers cards are #{dealer_cards}"
+  puts "Dealers total is #{dealer_total}"
+end
+
+def print_players_hand(player_cards, player_total)
+  say "Your cards are #{player_cards}"
+  puts "Your total is #{player_total}"
+end
+
+def blackjack?(player_total, dealer_total, dealer_cards)
+  if dealer_total == 21 && player_total == 21
+    print_dealers_hand(dealer_cards, dealer_total)
     say "Push! Dealer and Player have BlackJack!"
-  elsif d_total == 21
+    exit
+  elsif dealer_total == 21
+    print_dealers_hand(dealer_cards, dealer_total)
     say "Dealer has BlackJack Dealer Wins!"
-  elsif p_total == 21
+    exit
+  elsif player_total == 21
+    print_dealers_hand(dealer_cards, dealer_total)
     say "You have a BlackJack you Win!"
+    exit
   else
     "No Black Jack"
+  end
+end
+
+def busted?(player_total, dealer_total, dealer_cards)
+  if player_total > 21
+    say "You Busted! Sorry you lost"
+    print_dealers_hand(dealer_cards, dealer_total)
+    exit
+  elsif dealer_total > 21
+    say "Dealer Busted! You Win!"
+    print_dealers_hand(dealer_cards, dealer_total)
+    exit
+  end
+end
+
+def check_winner(player_total, dealer_total)
+  if dealer_total == player_total
+    say "Push! No Winners"
+  elsif dealer_total > player_total
+    say "Dealer Wins!"
+  else
+    say "You Win!"
   end
 end
 
@@ -60,13 +97,9 @@ player_total = calculate_total(player_cards)
 dealer_total = calculate_total(dealer_cards)
 
 say "The dealers first card is #{dealer_cards[0]}"
-say "Your cards are #{player_cards}"
-puts "Your total is #{player_total}"
+print_players_hand(player_cards, player_total)
 
-if blackjack?(player_total, dealer_total) != "No Black Jack"
-  puts "Game over"
-  exit
-end
+blackjack?(player_total, dealer_total, dealer_cards)
 
 while player_total < 21
   ask "Hit or Stand? Enter H for Hit or S for Stand"
@@ -74,56 +107,24 @@ while player_total < 21
   if hit_or_stand == "h"
     player_cards << play_deck.pop
     player_total = calculate_total(player_cards)
-    
-    say "Player hand #{player_cards}"
-
-    puts "Your total is now #{player_total}"
-
+    print_players_hand(player_cards, player_total)
+    busted?(player_total, dealer_total, dealer_cards)
   elsif hit_or_stand == "s"
     puts "You chose to stand"
     break
   end
 end
 
-if player_total > 21
-  say "You Busted! Sorry you lost"
-  exit
-end
-
-say "Dealers cards are #{dealer_cards}"
-
 while dealer_total < 17
+  print_dealers_hand(dealer_cards, dealer_total)
   say "Dealer chose to hit"
   dealer_cards << play_deck.pop
   say "Dealers hand is #{dealer_cards}"
   dealer_total = calculate_total(dealer_cards)
   puts "The Dealers new total is #{dealer_total}"
+  busted?(player_total, dealer_total, dealer_cards)
 end
 
-if dealer_total > 21
-  say "Dealer Busts! You Win!"
-  exit
-end
-
-puts "Your total is #{player_total}"
-
-if dealer_total == player_total
-  say "Push! No Winners"
-elsif dealer_total > player_total
-  say "Dealer Wins!"
-else
-  say "You Win!"
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
+print_dealers_hand(dealer_cards, dealer_total)
+print_players_hand(player_cards, player_total)
+check_winner(player_total, dealer_total)
